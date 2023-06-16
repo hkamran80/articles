5b29bbcb7f3458fed225868fe84c930be58d20d8
H. Kamran <hkamran@hkamran.com>
2023-06-15 23:32:13 -0700

    Remove debug logging

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -55,13 +55,6 @@ const jsonChanges = (
     )
 ).stdout;
 
-console.log("MD");
-console.log(markdownChanges);
-console.log("DEL");
-console.log(deletedMarkdown);
-console.log("JSON");
-console.log(jsonChanges);
-
 const paths = new Set();
 
 if (markdownChanges.trim().replace("\n", "") !== "") {
